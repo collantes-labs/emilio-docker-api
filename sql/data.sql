c611deaa8652bff751834e7e32b0d9cacf38f1ba
meltingghost <emiliodavidcc@gmail.com>
2023-12-26 16:00:48 -0400

    feat: api created

---
+++ sql/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (product_name, product_price, product_desc)
+VALUES ('Product 1', 10.99, 'Description product 1');
+
+INSERT INTO products (product_name, product_price, product_desc)
+VALUES ('Product 2', 15.50, 'Description product 2');
+
+INSERT INTO products (product_name, product_price, product_desc)
+VALUES ('Product 3', 8.25, 'Description product 3');
+
+INSERT INTO products (product_name, product_price, product_desc)
+VALUES ('Product 4', 12.75, 'Description product 4');
